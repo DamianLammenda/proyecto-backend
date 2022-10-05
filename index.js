8cfe6b32d8ce55af8cfc1cb317ca85a3dc33e450
Damian Lammenda <damianlammenda@hotmail.com>
2022-10-04 21:27:22 -0300

    tpClases

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 class Usuario {
-  constructor(nombre, apellido, libros, autor, mascotas) {
+  constructor(nombre, apellido, libro, autor, mascotas) {
     this.nombre = nombre;
     this.apellido = apellido;
-    this.libros = [{ libros, autor }];
+    this.libros = [{ libro, autor }];
     this.mascotas = [mascotas];
   }
 
   getFullName() {
-    return `Nombre completo ${this.nombre} ${this.apellido}`;
+    return `Nombre completo: ${this.nombre} ${this.apellido}`;
   }
   addMascota(mascotas) {
     return this.mascotas.push(mascotas);
@@ -16,23 +16,23 @@ class Usuario {
   countMascotas() {
     return this.mascotas.length;
   }
-  addLibro(libros, autor) {
-    this.libros.push({ libros, autor });
+  addLibro(libro, autor) {
+    this.libros.push({ libro, autor });
   }
   getBookNames() {
     return this.libros.map((books) => {
-      return books.libros;
+      return books.libro;
     });
   }
 }
 
 const damian = new Usuario ("Damian", "Lammenda", "It","Stephen King" ,"Iguana")
 const damianFullName = damian.getFullName()
-const damianLibros = damian.getBookNames()
 const addBook = damian.addLibro("El señor de los anillos", "J.R.R. Tolkien") 
 const addBook2 = damian.addLibro( "Carrie", "Stephen King") 
 const damianMascota = damian.addMascota ("Perro")
 const countMascotas = damian.countMascotas()
+const damianLibros = damian.getBookNames()
 
-console.log (damian);
+console.log (damianLibros);
 

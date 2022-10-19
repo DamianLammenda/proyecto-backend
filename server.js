b424d0411b52edcb8f4c111ca45898a88465bfd6
Damian Lammenda <damianlammenda@hotmail.com>
2022-10-19 09:24:18 -0300

    fixTP3

--- server.js
+++ server.js
@@ -1,40 +1,23 @@
 const express = require('express');
-const fs = require('fs');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3004;
 
-// const Contenedor = require('./tp2');
-// const contenedor = new Contenedor("productos.txt");
+ const Contenedor = require('./tp2');
+ const contenedor = new Contenedor("productos.txt");
 
-//devuelve un array con los productos.txt
-app.get('/productos', (_req, res) => {
-    res.status(200).send(fs.readFileSync('productos.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-            }
-            return data;
-            }));
-});
-
-// app.get('/productos', (_req, res) => {
-//     res.send('Hola');
-// });
-// app.get('/productosRandom', (_req, res) => {
-//     res.status(200).send('Hola random');
-// });
-//devuelve un producto aleatorio del array de productos.txt
-app.get('/productosRandom', (_req, res) => {
-    res.status(200).send(fs.readFileSync('productos.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            }
-            const productos = JSON.parse(data);
-            const random = Math.floor(Math.random() * productos.length);
-            return productos[random];
-            }));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ 
+ app.get('/productos', async (_req, res) => {
+        const productos = await contenedor.getAll();
+        res.status(200).json(productos);
+    });
+app.get('/productosRandom', async (_req, res) => {
+    const productos = await contenedor.getAll();
+    const random = Math.floor(Math.random() * productos.length);
+    res.status(200).json(productos[random]);
 });
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-

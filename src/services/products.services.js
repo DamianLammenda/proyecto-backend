002e76b2cc254dfa74a0560af7cbae4b84f11138
Damian Lammenda <damianlammenda@hotmail.com>
2022-11-01 23:33:56 -0300

    fixtp5

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -3,13 +3,13 @@
 let products = [
   {
     name: "Bitcoin",
-    price: 20.000,
+    price: 20000,
     thumbnail:
       "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/bitcoincash_bch_bitcoin-64.png",
   },
   {
     name: "Ethereum",
-    price: 1.500,
+    price: 1500,
     thumbnail:"https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/etherium_eth_ethcoin_crypto-64.png"
   }
 ];
